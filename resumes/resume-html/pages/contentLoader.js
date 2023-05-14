c00a5ecd60ed95a7341359fe6a644b67700a3113
SONGHYEON JUN <shjeon0730@gmail.com>
2023-05-14 10:14:32 -0500

    first add

---
+++ resumes/resume-html/pages/contentLoader.js
@@ -0,0 +1,25 @@
+
+const { LeftContents: __LeftContents, TopContents: __TopContents, RightContents: __RightContents } = await import( "./consts.js?version="+getVariant());
+
+/**
+ * LeftContents
+ * @type {{
+ *  Introduction: {TITLE:string, DESC:string},
+ *  Careers: {TITLE:string, CAREERS:[CareerType] } 
+ * }}
+ */
+export const  LeftContents = __LeftContents;
+
+export const  TopContents = __TopContents;
+
+/**
+ * @type {{
+ * skillLevels: Object<string, [SkillObj]>,
+ * volunteers: [VolunteerObj],
+ * educations: [EducationObj],
+ * awards: [AwardObj],
+ * authority: [string],
+ * achievements: [{company:string, items:[AchievementObj]}]
+ * }}
+ */
+ export const  RightContents = __RightContents;
